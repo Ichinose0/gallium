dcd9ec59c461550fdc9a80c3dc61ace79b968cb0
Ichinose0 <148521661+Ichinose0@users.noreply.github.com>
2023-12-12 14:31:46 +0900

    Add documentation to GMResult

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,14 @@ pub use instance::*;
 pub use pipeline::*;
 pub use queue::*;
 
+/// Result value returned when gallium instruction is executed
+///
+/// # Value Meaning
+/// * `IncompatibleDriver` - Unsupported driver is used.
+/// * `InitializationError` - Initialization failed for some reason. 
+/// * `InvalidValue` - Invalid value passed.
+/// * `OutOfMemory` - Out of memory.
+/// * `UnknownError` - Unknown error.
 #[derive(Clone, Copy, Debug)]
 pub enum GMResult {
     Success,
